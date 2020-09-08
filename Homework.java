fcf998454c7e79291d216d373dab9da169594dfc
et <et@qq.com>
2020-09-08 11:18:12 +0800

    在公司开发的ZY

--- Homework.java
+++ Homework.java
@@ -1,4 +1,9 @@
 public class Homework{
 	private int id;
 	private String name;
+	private int age;
+
+	private void work(){
+
+	}
 }
--- ZY.java
+++ ZY.java
@@ -1,3 +1,5 @@
 public class ZY{
 	private String name;
+	private int id;
+	private int age;
 }
